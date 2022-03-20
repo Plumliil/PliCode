20354e4175a1b3d3a4ab323ded3abc932a127733
plumli <476453574@qq.com>
2022-03-20 15:11:42 +0800

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,17 @@ module.exports={
     // 模式
     mode:'development',
     // 入口
-    entry:'./src/index.js',
+    entry:'./index.js',
     // 出口
     output:{
         // 打包文件夹
         path:path.resolve(__dirname,'dist'),
         // 打包文件
-        filename:'pliUtils.js',
+        filename:'bundle.js',
         // 向外暴露的对象名称
-        library:'pliUtils',
+        library:'PliLib',
         // 打包生成库可以通过esm/commonjs/requirejs的语法引入
-        libraryTarget:'umd',
+        libraryTarget:'this',
     }
 
     
